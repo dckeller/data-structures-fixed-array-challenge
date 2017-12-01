3c1421698b858454d7759198b5a7c1c78da907c0
David Keller <dckeller1@gmail.com>
2017-12-01 09:46:20 -0700

    Add testing, and fix errors for FixedArray class

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,28 @@
 class FixedArray
 
+	def initialize(size)
+		@size = size
+		@array = Array.new(@size)
+	end
+
+	def get(index)
+		if index > array.length -1
+			puts "OutOfBoundsException"
+		else 
+			array[index]
+		end 
+	end
+
+	def set(index, element)
+		if index > array.length - 1
+			puts "OutOfBoundsException"
+		else 
+			array.insert(index, element)
+			array
+		end 
+	end  
+
 end
+
+new_array = FixedArray.new(5)
+puts new_array
